3bb5c0b66513376a304ae0c3a2aed8d0f9e60edc
bartstonejjj <bartstonejjj@gmail.com>
2015-12-23 18:49:03 +0000

    removed extraneous comments and variables etc

--- sudoku.py
+++ sudoku.py
@@ -2,10 +2,12 @@ import fileinput
 
 # A single cell in a SuDoKu table
 class Cell(object):
+	# size of cell in this table
 	size = 9
+	
 	# x = x position in SuDoKu table starting from 0 on left (Row)
 	# y = y position in SuDoKu table starting from 0 on top (Col)
-	# subBox (see SubBox class)
+	# subBox  = positino of subbox table, starting from 0 in top left, then counting left to right, top to bottom
 	def __init__(self, x, y):
 		self._x = x
 		self._y = y
@@ -27,6 +29,7 @@ class Cell(object):
 			return "    "+str(self.cellValue())+"    "
 		return st
 	
+	# Get the subbox number from the row and column (x, y)
 	@staticmethod
 	def get_subbox(x, y):
 		return 3 * (y/3) + (x/3)
@@ -42,10 +45,6 @@ class Cell(object):
 	@property
 	def cellValueUnknown(self):
 		return self._cellValueUnknown
-
-	@cellValueUnknown.setter
-	def cellValueUnknown(self, value):
-		self._cellValueUnknown = value
 
 	# Returns the value of this Cell if sole value has been found, otherwise returns 0
 	def cellValue(self):
@@ -66,12 +65,11 @@ class Cell(object):
 		self.values[value-1] = 0
 		return self.cellValue()
 
-	# Is called when a value has been found, and checks off all other cells in the subsections
+	# Is called when a value has been found, it checks off all other cells in the subsections
 	def valueFound(self, value):
 		# If no value has been found yet
 		if self.cellValueUnknown:
-			print "Value Found "+" "+str(value)+ " at "+str(self.x)+", "+str(self.y)
-			self.cellValueUnknown = False
+			self._cellValueUnknown = False
 
 			# update each subsection removing the rows and columns
 			rows[self.x].updateSection(self, value)
@@ -88,7 +86,7 @@ class Cell(object):
 		self.values[value - 1] = 1
 		self.valueFound(value)
 	
-
+# This class to store 9 cells of one of: row, column or subbox
 class SubSection(object):
 	def __init__(self):
 		self.Cells = []
@@ -107,45 +105,21 @@ class SubSection(object):
 	def updateSection(self, cell, value):
 		# Iterate through all cells in sub-section, except for 'cell'
 		for c in self.Cells:
-			#s = "c = "+str(c)+" cell = "+str(cell)+" NOT SAME ="+str(c != cell)
-			#print s
-			try:
-				if c != cell:
+			if c != cell:
 
-					if c.cellValueUnknown:
-						# Remove 'value' from possible values of this cell
-						# Store the result of whether a sole value of this cell has been found as a result of this
-						val = c.removeValue(value)
+				if c.cellValueUnknown:
 
-						# If a sole value has been found, check other corresponding subections recursively.
-						# (Note for later) Might be able to move this bit into removeValue itself.
-						if val:
-							#print "Test Value Found "+" "+str(val)+ " at "+str(c.x)+", "+str(c.y)
-							c.valueFound(val)
+					# Remove 'value' from possible values of this cell
+					# Store the result of whether a sole value of this cell has been found as a result of this
+					val = c.removeValue(value)
 
-			except RuntimeError as re:
-				if re.args[0] != 'maximum recursion depth exceeded':
-				# different type of runtime error
-					print('Sorry but this was not able to finish {}'.format(re.args[0]))
+					# If a sole value has been found, check other corresponding subections recursively.
+					# (Note for later) Might be able to move this bit into removeValue itself.
+					if val:
+						c.valueFound(val)
 
 	def get_cell(self, index):
 		return self.Cells[index]
-
-# A row in a SuDoKu table starting from 0 at the top
-class Row(SubSection):
-	def __init__(self, values):
-		super(Row, self).__init__(values)
-
-# A column in a SuDoKu table starting from 0 on the left
-class Col(SubSection):
-	def __init__(self, values):
-		super(Col, self).__init__(values)
-
-# A sub box (square) in a SuDoKu table starting from 0 on the top left
-# then coounting from left to right, top to bottom
-class SubBox(SubSection):
-	def __init__(self, values):
-		super(SubBox, self).__init__(values)
 
 # Create and initialise cell, row, col and subbox data structures
 global size, rows, cols, subboxes, allCells
@@ -153,7 +127,6 @@ rows = []
 cols = []
 subboxes = []
 allCells = []
-
 
 # Initialise sub section variables
 for i in range(Cell.size):
@@ -172,19 +145,15 @@ for i in range(Cell.size):
 		columns.append(cell)
 	allCells.append(columns)
 
+# Iterate through sudoku files and set values found recursively
 i = 0
 for line in fileinput.input():
 	j = 0
 	while j < len(line) and j < Cell.size:
 		if line[j] != "-":
-			#s = "x = "+str(i)+", y = "+str(j)+", value = "+str(line[j])
-			#print s
-			Cell.recursiveCount = 0
 			allCells[i][j].setValueFound(int(line[j]))
 		j += 1
 	i += 1
-
-print "\n\n"
 
 # Print out SuDoKu table
 for i in range(Cell.size):
@@ -192,7 +161,3 @@ for i in range(Cell.size):
 		s = str(allCells[i][j])+" |"
 		print s,
 	print " "
-
-#for r in rows:
-#	print r
-#	print " "
